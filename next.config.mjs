bc622fda7e7c7bf2e5c566b83bfb0aa079a75327
blindjedi <matt6js@gmail.com>
2024-06-30 21:57:58 -0600

    add redirect to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/calculator',
+          permanent: true, // Set to true if this is a permanent redirect
+        },
+      ];
+    },
+  };
 
 export default nextConfig;
