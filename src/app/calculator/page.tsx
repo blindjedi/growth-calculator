e276db4267a9f108d220f1222e7b0e1bb60354a4
blindjedi <matt6js@gmail.com>
2024-07-04 16:16:48 -0600

    fix heigh of screen

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -99,7 +99,7 @@ const CompoundInterestCalculator: React.FC = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-4xl mx-auto p-4 min-h-screen flex flex-col">
       <h1 className="text-3xl font-bold mb-6 text-center">
         Compound Interest Calculator
       </h1>
