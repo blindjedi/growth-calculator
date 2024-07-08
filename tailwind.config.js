ff9f47c36f55ec9f10bbc15ad5239ad917d6c962
blindjedi <matt6js@gmail.com>
2024-07-07 21:58:41 -0600

    added daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [require("daisyui")],
   theme: {
     extend: {
       backgroundImage: {
