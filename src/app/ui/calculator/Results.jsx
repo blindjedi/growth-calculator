19e244fcb22329b193985bd9b65d11ed17729e30
blindjedi <matt6js@gmail.com>
2024-07-07 19:05:04 -0600

    added background to results

--- src/app/ui/calculator/Results.jsx
+++ src/app/ui/calculator/Results.jsx
@@ -5,17 +5,17 @@ const Results = ({ results, chartData, chartOptions }) => {
   if (!results) return null;
 
   return (
-    <>
-      <div className="mt-8">
-        <h2 className="text-3xl font-bold mb-4 text-center">Results</h2>
+    <div className="mt-8">
+      <h2 className="text-3xl font-bold mb-4 text-center">Results</h2>
+      <div className="bg-gray-100 p-4 rounded-lg shadow-md">
         <p className="text-center text-2xl mb-2">Final Balance: ${results.finalBalance.toLocaleString()}</p>
         <p className="text-center text-2xl mb-2">Total Contributions: ${results.totalContributions.toLocaleString()}</p>
         <p className="text-center text-2xl mb-6">Total Interest Earned: ${results.totalInterest.toLocaleString()}</p>
-        <div className="w-full h-96 flex justify-center my-6">
-          <Line options={chartOptions} data={chartData} />
-        </div>
       </div>
-    </>
+      <div className="w-full h-96 flex justify-center my-6">
+        <Line options={chartOptions} data={chartData} />
+      </div>
+    </div>
   );
 };
 
