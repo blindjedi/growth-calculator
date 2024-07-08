f62011623e80cfbaec62bd415017fed44edd9898
blindjedi <matt6js@gmail.com>
2024-07-07 21:59:05 -0600

    add daisy ui stats to results

--- src/app/ui/calculator/Results.jsx
+++ src/app/ui/calculator/Results.jsx
@@ -7,10 +7,21 @@ const Results = ({ results, chartData, chartOptions }) => {
   return (
     <div className="mt-8">
       <h2 className="text-3xl font-bold mb-4 text-center">Results</h2>
-      <div className="bg-gray-100 p-4 rounded-lg shadow-md dark:bg-gray-800">
-        <p className="text-center text-2xl mb-2">Final Balance: ${results.finalBalance.toLocaleString()}</p>
-        <p className="text-center text-2xl mb-2">Total Contributions: ${results.totalContributions.toLocaleString()}</p>
-        <p className="text-center text-2xl mb-6">Total Interest Earned: ${results.totalInterest.toLocaleString()}</p>
+      <div className="bg-gray-100 p-4 rounded-lg shadow-lg dark:bg-gray-800">
+        <div className="stats text-center">
+          <div className="stat">
+            <div className="stat-title">Final Balance</div>
+            <div className="stat-value text-2xl">${results.finalBalance.toLocaleString()}</div>
+          </div>
+          <div className="stat">
+            <div className="stat-title">Total Contributions</div>
+            <div className="stat-value text-2xl">${results.totalContributions.toLocaleString()}</div>
+          </div>
+          <div className="stat">
+            <div className="stat-title">Total Interest Earned</div>
+            <div className="stat-value text-2xl">${results.totalInterest.toLocaleString()}</div>
+          </div>
+        </div>
       </div>
       <div className="w-full h-96 flex justify-center my-6">
         <Line options={chartOptions} data={chartData} />
